20180036235c6fb50303fde1060f3efd18207b15
Marek Matejovic <me@devmaroy.cz>
2019-11-20 07:41:46 +0100

    Navigation query added

--- src/layout/Header/Navigation.js
+++ src/layout/Header/Navigation.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { graphql, StaticQuery } from 'gatsby';
+
+const query = graphql`
+{
+    allWordpressWpApiMenusMenusItems( filter: { name: { eq: "Main Menu" } } ) {
+      edges {
+        node {
+          items {
+            title
+            object_slug
+          }
+        }
+      }
+    }
+  }
+`;
 
 const Navigation = () => {
     return (
-        <div>
-            Navigation
-        </div>
+        <StaticQuery query={ query } render={ ( props ) => (
+            <div>
+                <ul>
+                    { props.allWordpressWpApiMenusMenusItems.edges[0].node.items.map( ( item ) => (
+                        <li key={ item.object_slug }>
+                            { item.title }
+                        </li>
+                    ))}  
+                </ul>
+            </div>
+        )} />
     );
 };
 
