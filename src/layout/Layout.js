39cb9f6991bc2df68c60b60937b3e9a7aefe0498
Marek Matejovic <me@devmaroy.cz>
2019-11-20 07:16:32 +0100

    Better folder structure for Header component

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import { ThemeProvider, createGlobalStyle } from 'styled-components';
 import { Normalize } from 'styled-normalize';
 import theme from '../theme/theme';
-import Header from './Header';
+import Header from './Header/Header';
 
 const GlobalStyles = createGlobalStyle`
     body {
