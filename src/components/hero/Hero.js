f38b721666f8042cd4964b6ec07c63227aed6ef5
Marek Matejovic <me@devmaroy.cz>
2019-11-21 14:17:08 +0100

    Hero component done

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const HeroWrapper = styled.div`
     background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), ${ props => `url( ${ props.image } )`} center center/cover no-repeat;
-    margin: 8rem 0;
+    margin: 6rem 0;
     padding: 4rem;
     display: flex;
     flex-direction: column;
