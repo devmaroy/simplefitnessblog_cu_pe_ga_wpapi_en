81a130412a94d1893dce2a5f9bc47eda004cc8d6
Marek Matejovic <me@devmaroy.cz>
2019-11-21 18:05:30 +0100

    Font settings for theme

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -3,6 +3,32 @@ const colors = {
     background: '#FFF'
 }
 
+
+// Fonts
+const fonts = {
+    primary: 'Karla, sans-serif',
+    secondary: 'Rubik, sans-serif',
+    sizes: {
+        normal: '1.6rem',
+        smaller: '1.4rem',
+        small: '1.2rem'
+    },
+    weights: {
+        light: 300,
+        normal: 400,
+        medium: 500,
+        semiBold: 600,
+        bold: 700,
+        elements: {
+            heading: 700,
+            text: 400,
+            blockquote: 300
+        }
+    }
+}
+
+
 export default {
     colors,
+    fonts,
 }
