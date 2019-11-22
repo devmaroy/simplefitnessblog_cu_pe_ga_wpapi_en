9986eee2e1cfdadbcf6298882d12268afafb8775
Marek Matejovic <me@devmaroy.cz>
2019-11-22 16:57:04 +0100

    Footer component done

--- src/layout/footer/Footer.js
+++ src/layout/footer/Footer.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
+import Container from '../Container';
+import SiteLogo from '../header/SiteLogo';
+
+const FooterWrapper = styled.footer`
+    margin-top: 8rem;
+    margin-bottom: 2rem;
+`;
+
+const FooterInfo = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const Copyright = styled.p`
+    margin: 0;
+    color: ${ props => props.theme.colors.lightGray };
+
+    &::before {
+        content: '\u00A9 ';
+    }
+`;
 
 const Footer = () => {
     return (
-        <div>
-            footer is here voe
-        </div>
+        <FooterWrapper>
+            <Container>
+                <FooterInfo>
+                    <SiteLogo />
+                    <Copyright>2019 All Rights Reserved.</Copyright>
+                </FooterInfo>
+            </Container>
+        </FooterWrapper>
     );
 };
 
