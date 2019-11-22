1e8668ffd69d67140468be2b4c3b44ac3629f63c
Marek Matejovic <me@devmaroy.cz>
2019-11-22 07:02:03 +0100

    Hero component refactor

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -8,48 +8,35 @@ const HeroWrapper = styled.div`
     justify-content: center;
     min-height: 45rem;
     padding: 4rem;
+    background: ${ props => props.theme.colors.overlayLinearGradient }, ${ props => `url(${ props.image })` } center center/cover no-repeat;
     color: ${ props => props.theme.colors.light };
+`;
+
+const Heading = styled.h1`
+    font-family: ${ props => props.theme.fonts.secondary };
+    font-size: 4.2rem;
+    font-weight: ${ props => props.theme.fonts.weights.bold };
+    line-height: 5rem;
+`;
+
+const Content = styled.div`
+    font-size: ${ props => props.theme.fonts.sizes.normal };
+    line-height: 2.9rem;
+
+    p {
+        margin: 0;
+    }
 `;
 
 const Hero = ( { data } ) => {
     return (
         <HeroWrapper image={ data.featured_media.source_url }>
             <Container>
-                <h1>{ data.title }</h1>
-                <div dangerouslySetInnerHTML={ { __html: data.content } } />
+                <Heading>{ data.title }</Heading>
+                <Content dangerouslySetInnerHTML={ { __html: data.content } } />
             </Container>
         </HeroWrapper>
     )
 }
 
 export default Hero;
-
-
-
-
-/*import React from 'react';
-import styled from 'styled-components';
-import Container from '../../layout/Container';
-
-const HeroWrapper = styled.div`
-    background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), ${ props => `url( ${ props.image } )`} center center/cover no-repeat;
-    padding: 4rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    color: #fff;
-    min-height: 45rem;
-`;
-
-const Hero = ( { data } ) => {
-    return (
-        <HeroWrapper image={ data.featured_media.source_url }>
-            <Container>
-                <h1>{ data.title }</h1>
-                <div dangerouslySetInnerHTML={ { __html: data.content } } />
-            </Container>
-        </HeroWrapper>
-    );
-};
-
-export default Hero;*/
