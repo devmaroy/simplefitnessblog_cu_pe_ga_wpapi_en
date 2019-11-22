ac55b5f85c0431f4b81a310e63e758c758d71858
Marek Matejovic <me@devmaroy.cz>
2019-11-22 13:24:20 +0100

    SiteLogo component refactor

--- src/layout/Header/SiteLogo.js
+++ src/layout/Header/SiteLogo.js
@@ -4,36 +4,35 @@ import styled from 'styled-components';
 
 const Logo = styled.a`
 	img {
-		width: 200px;
+		width: 20rem;
 	}
 `;
 
 const query = graphql`
 {
-    allWordpressWpLogo {
-      edges {
-        node {
-          url {
-            source_url
-          }
-        }
-      }
-    }
-  }
+	allWordpressWpLogo {
+		edges {
+			node {
+				url {
+					source_url
+				}
+			}
+		}
+	}
+}
 `;
 
+
 const SiteLogo = () => {
-    return (
-        <StaticQuery query={ query } render={ ( props ) => (
-            <div>
-                <Logo href="/">
-                  <img src="/logo.svg" alt=""/>
-                </Logo>
-            </div>
-        )} />
-    );
-};
+	return (
+		<StaticQuery query={ query } render={ ( props ) => (
+			<div style={{ lineHeight: 0 }}>
+				<Logo href="/">
+					<img src="/logo.svg" alt="Site logo" />
+				</Logo>
+			</div>
+		) } />
+	)
+}
 
 export default SiteLogo;
-
-//<img src={ props.allWordpressWpLogo.edges[0].node.url.source_url } alt="Logo" width="200" />
