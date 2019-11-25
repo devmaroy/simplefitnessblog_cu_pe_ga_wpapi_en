4cd5f243490e8a9cac7bf1a3ba490ec0a6c3eba5
Marek Matejovic <me@devmaroy.cz>
2019-11-25 15:01:52 +0100

    Social component done

--- src/layout/Header/navigation/Social.js
+++ src/layout/Header/navigation/Social.js
@@ -1,31 +1,64 @@
 import React from 'react';
 import styled from 'styled-components';
+import { transparentize } from 'polished';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 
 // Styles
 
 const SocialList = styled.ul`
-   	margin: 0;
+   	margin: 6rem 0 0 0;
 	padding: 0;
 	list-style: none; 
 `;
 
-const SocialLink = styled.a`
+const SocialListItem = styled.li`
+    display: inline-block;
+    margin-right: 2rem;
+`;
 
+const SocialLink = styled.a`
+    color: ${ props => transparentize( 0.2, props.theme.colors.light ) };
+
+    transition: ${ props => props.theme.transitions.link };
+	
+	&:hover,
+	&:focus {
+		color: ${ props => props.theme.colors.light };
+	}
+
+    svg {
+        font-size: 2rem;
+    }
 `;
 
 const Social = () => {
     return (
         <SocialList>
-            <li>
-                <SocialLink href="/">Twitter</SocialLink>
-            </li>
-            <li>
-                <SocialLink href="/">Facebook</SocialLink>
-            </li>
-            <li>
-                <SocialLink href="/">Instagram</SocialLink>
-            </li>
+            <SocialListItem>
+                <SocialLink href="/">
+                    <FontAwesomeIcon 
+                        icon={["fab", "twitter"]}
+                        fixedWidth 
+                    />
+                </SocialLink>
+            </SocialListItem>
+            <SocialListItem>
+                <SocialLink href="/">
+                    <FontAwesomeIcon 
+                        icon={["fab", "facebook-f"]} 
+                        fixedWidth
+                    />
+                </SocialLink>
+            </SocialListItem>
+            <SocialListItem>
+                <SocialLink href="/">
+                    <FontAwesomeIcon 
+                        icon={["fab", "instagram"]} 
+                        fixedWidth
+                    />
+                </SocialLink>
+            </SocialListItem>
         </SocialList>
     );
 };
